c980f13b11c49a1b984d4a20ee07ba098c48a470
Karol <karolma02@gmail.com>
2025-06-17 12:06:47 +0200

    slider

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -11,6 +11,11 @@ class OptionsPage extends StatefulWidget {
 }
 
 class _OptionsPageState extends State<OptionsPage> {
+  bool btnTest1 = false;
+  bool btnTest2 = false;
+  bool btnTest3 = false;
+
+  double limitSlider = 1500;
 
   @override
   void initState() {
@@ -48,24 +53,68 @@ class _OptionsPageState extends State<OptionsPage> {
           ListTile(
             title: const Text('Test 1'),
             trailing: Switch(
-              value: false,
-              onChanged: (_) {},
+              value: btnTest1,
+              onChanged: (val) {
+                setState(() {
+                  btnTest1 = val;
+                });
+              },
             ),
           ),
           ListTile(
             title: const Text('Test 2'),
             trailing: Switch(
-              value: false,
-              onChanged: (_) {},
+              value: btnTest2,
+              onChanged: (val) {
+                setState(() {
+                  btnTest2 = val;
+                });
+              },
             ),
           ),
           ListTile(
             title: const Text('Test 3'),
             trailing: Switch(
-              value: false,
-              onChanged: (_) {},
+              value: btnTest3,
+              onChanged: (val) {
+                setState(() {
+                  btnTest3 = val;
+                });
+              },
             ),
           ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Testowy slider:',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                '${limitSlider.toStringAsFixed(0)} PLN',
+                style: const TextStyle(fontSize: 14),
+              ),
+              Slider(
+                min: 0,
+                max: 10000,
+                divisions: 100,
+                label: limitSlider.toStringAsFixed(0),
+                value: limitSlider,
+                onChanged: (value) {
+                  setState(() {
+                    limitSlider = value;
+                  });
+                },
+                onChangeEnd: (_) {
+                  // _saveThreshold();
+                },
+              ),
+            ],
+          )
         ],
       ),
     );
